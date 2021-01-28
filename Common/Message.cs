1b45c523c0dd3734d27fd3203e1a322ff73a9857
Helder Silva <a13009@alunos.ipca.pt>
2021-01-28 14:47:22 +0000

    final

--- Common/Message.cs
+++ Common/Message.cs
@@ -19,12 +19,14 @@ namespace Common
         public float X { get; set; }
         public float Y { get; set; }
         public float Z { get; set; }
+
         #region CubePositions
         public float C1x { get; set; }
         public float C1y { get; set; }
         public float C1z { get; set; }
         
         #endregion
+
         #region portalPositions
         public float P1x { get; set; }
         public float P2x { get; set; }
@@ -33,6 +35,7 @@ namespace Common
         public float P1z { get; set; }
         public float P2z { get; set; } 
         #endregion
+
         public float lookX { get; set; }
         public float lookY { get; set; }
         public float lookZ { get; set; }
